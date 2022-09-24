41146e01a731cf906e9005abbc9039ee06653284
Noah Kim <noah7203@berkeley.edu>
2022-09-23 19:28:45 -0700

    MaxArrayDeque first version

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,7 +1,41 @@
 package deque;
 
-public class MaxArrayDeque<T> extends ArrayDeque {
-    public MaxArrayDeque() {
+import java.util.Comparator;
 
+public class MaxArrayDeque<T> extends ArrayDeque {
+    private Comparator<T> comparator;
+    public MaxArrayDeque(Comparator<T> c) {
+        super();
+        comparator = c;
+    }
+
+    public T max() {
+        if (this.size() == 0) {
+            return null;
+        }
+        T max = (T) this.get(0);
+        for (int i = 1; i < this.size(); i++) {
+            if (comparator.compare((T) this.get(i), max) > 0) {
+                max = (T) this.get(i);
+            } else {
+                continue;
+            }
+        }
+        return max;
+    }
+
+    public T max(Comparator<T> c) {
+        if (this.size() == 0) {
+            return null;
+        }
+        T max = (T) this.get(0);
+        for (int i = 1; i < this.size(); i++) {
+            if (c.compare((T) this.get(i), max) > 0) {
+                max = (T) this.get(i);
+            } else {
+                continue;
+            }
+        }
+        return max;
     }
 }
