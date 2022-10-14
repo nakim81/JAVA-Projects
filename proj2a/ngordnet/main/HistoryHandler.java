f6661cf486fb90a6d9d6f9d4db03fa2df35647a0
Noah Kim <noah7203@berkeley.edu>
2022-10-13 19:20:09 -0700

    .

--- proj2a/ngordnet/main/HistoryHandler.java
+++ proj2a/ngordnet/main/HistoryHandler.java
@@ -1,4 +1,5 @@
 package ngordnet.main;
+
 import ngordnet.hugbrowsermagic.NgordnetQuery;
 import ngordnet.hugbrowsermagic.NgordnetQueryHandler;
 import ngordnet.ngrams.*;
@@ -6,19 +7,27 @@ import ngordnet.plotting.Plotter;
 import org.knowm.xchart.XYChart;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryHandler extends NgordnetQueryHandler {
     private NGramMap myMap;
+
     public HistoryHandler(NGramMap map) {
         myMap = map;
     }
+
     @Override
     public String handle(NgordnetQuery q) {
 
         ArrayList<TimeSeries> lts = new ArrayList<>();
         ArrayList<String> labels = new ArrayList<>();
-        for (String word : q.words()) {
+        List<String> words = q.words();
+        for (String word : words) {
             labels.add(word);
+            //lts.add(myMap.weightHistory(word));
+        }
+
+        for (String word : words) {
             lts.add(myMap.weightHistory(word));
         }
 
